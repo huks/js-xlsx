004b47141ec32f3500a556ca471003e0c6f69de1
huks <huks7417@gmail.com>
2017-03-07 15:13:53 +0900

    the_get added

--- kiwi.js
+++ kiwi.js
@@ -21,7 +21,8 @@ function work_header(ws) {
 	work_cell(ws, "S1", "RACK NUMBER"); // rack number
 }
 
-function work_cell(worksheet, address, value) {
+/* NOT USED */
+function work_cell_foo(worksheet, address, value) {
 	/*
 	 * v: raw value
 	 * w: formatted text (if applicable)
@@ -35,12 +36,20 @@ function work_cell(worksheet, address, value) {
 	w_cell.v = value;
 }
 
-/* NOT USED */
-// function format_text_cell(worksheet, address, value) {
-// 	var f_address = address;
-// 	var f_cell = worksheet[f_address];
-// 	f_cell.w = value;
-// }
+function work_cell(worksheet, address, value) {
+
+	var w_address = address;
+	if (worksheet[w_address] == null) {
+		worksheet[w_address] = {v:"",t:"s"};
+		var w_cell = worksheet[w_address];
+		w_cell.v = value;
+	} else {
+		var w_cell = worksheet[w_address];
+		w_cell.t = "s";
+		w_cell.v = value;
+	}
+
+}
 
 function convert_cmate(workbook) {
 	/* Get worksheet */
@@ -60,44 +69,63 @@ function convert_cmate(workbook) {
 	for(i=2;i<=cmate_row_length;i++){
 		/* external order number : order number(A) */
 		work_cell(ws, "A"+[i], ws_origin["A"+[i]].w);
+
 		/* barcode : WIP */
 		work_cell(ws, "B"+[i], "BARCODE");
+
 		/* courier company : logistics companies(O) */
 		work_cell(ws, "C"+[i], ws_origin["O"+[i]].w);
+
 		/* recipient name : recipient(I) */
 		work_cell(ws, "D"+[i], ws_origin["I"+[i]].w);
+
 		/* recipient id number : WIP */
 		work_cell(ws, "E"+[i], "RECIPIENT_ID_NUMBER"); 		
+
 		/* recipient province : provincial cities and counties(J) */
 		var string = ws_origin["J"+[i]].w;
 		var strArray = string.split(" ");
 		work_cell(ws, "F"+[i], strArray[0]);
+
 		/* recipient city : strArray[1] */
 		work_cell(ws, "G"+[i], strArray[1]);
+
 		/* recipient country : strArray[2] */
 		work_cell(ws, "H"+[i], strArray[2]);
+
 		/* recipient recipient street and house number : address(K) */
 		work_cell(ws, "I"+[i], ws_origin["K"+[i]].w);
+
 		/* recipient contact phone : phone(M) */
 		work_cell(ws, "J"+[i], ws_origin["M"+[i]].w);
+
 		/* mail address : zip code(N) */
 		work_cell(ws, "K"+[i], ws_origin["N"+[i]].w);
+
 		/* package weight (KG) : WIP */
 		work_cell(ws, "L"+[i], "");
+
 		/* item unit price (RMB) : price(S) */
 		work_cell(ws, "M"+[i], ws_origin["S"+[i]].w);
+
 		/* number of the stuffs : real quantity(U) */
 		work_cell(ws, "N"+[i], ws_origin["U"+[i]].w);
+
 		/* payment method : WIP */
 		work_cell(ws, "O"+[i], "");
+
 		/* product name : platform product name(R) */
 		work_cell(ws, "P"+[i], ws_origin["R"+[i]].w);
+
 		/* order generation time : transaction hour(F) */
 		work_cell(ws, "Q"+[i], ws_origin["F"+[i]].w);
+
 		/* purchaser platform id : WIP */
 		work_cell(ws, "R"+[i], "");
+
 		/* rack number : shipment number(P) */
 		work_cell(ws, "S"+[i], ws_origin["P"+[i]].w);
+
 		/* empty cells */
 		work_cell(ws, "T"+[i], "");
 		work_cell(ws, "U"+[i], "");
@@ -113,52 +141,69 @@ function convert_the_get(workbook) {
 	/* Copy worksheet */
 	var ws_origin = ws;
 
-	var get_row_length = ws['!ref'].substr(4);
-	console.log("get_row_length: " + get_row_length);
+	var the_get_row_length = ws['!ref'].substr(4);
+	console.log("the_get_row_length: " + the_get_row_length);
 
 	work_header(ws);
 
-	console.log("hello");
-
-	for(i=2;i<=get_row_length;i++){
+	for(i=2;i<=the_get_row_length;i++){
 		/* external order number : 订单编号 몰 주문번호(A) */
 		work_cell(ws, "A"+[i], ws_origin["A"+[i]].w);
+
 		/* barcode : WIP */
 		work_cell(ws, "B"+[i], "BARCODE");
+
 		/* courier company : WIP */
 		work_cell(ws, "C"+[i], "COURIER_COMPANY");
+
 		/* recipient name : 收货人姓名 수화인성명(G) */
 		work_cell(ws, "D"+[i], ws_origin["G"+[i]].w);
+
 		/* recipient id number : 买家会员名 회원아이디(D) */
 		work_cell(ws, "E"+[i], ws_origin["D"+[i]].w); 		
+
 		/* recipient province : WIP */
 		work_cell(ws, "F"+[i], "");
+
 		/* recipient city : WIP */
 		work_cell(ws, "G"+[i], "");
+
 		/* recipient country : WIP */
 		work_cell(ws, "H"+[i], "");
+
 		/* recipient recipient street and house number : 收货地址 주소(H) */
 		work_cell(ws, "I"+[i], ws_origin["H"+[i]].w);
+
 		/* recipient contact phone : 联系手机 연락방식(I) */
-		work_cell(ws, "J"+[i], ws_origin["M"+[i]].w);
+		work_cell(ws, "J"+[i], ws_origin["I"+[i]].w);
+
 		/* mail address : WIP */
 		work_cell(ws, "K"+[i], "");
+
 		/* package weight (KG) : WIP */
 		work_cell(ws, "L"+[i], "");
+
 		/* item unit price (RMB) : WIP */
 		work_cell(ws, "M"+[i], "");
+
 		/* number of the stuffs : 购买数量 수량(K) */
 		work_cell(ws, "N"+[i], ws_origin["K"+[i]].w);
+
 		/* payment method : WIP */
 		work_cell(ws, "O"+[i], "");
+
 		/* product name : 标题 품명(J) */
 		work_cell(ws, "P"+[i], ws_origin["J"+[i]].w);
+
 		/* order generation time : WIP */
 		work_cell(ws, "Q"+[i], "");
+
 		/* purchaser platform id : WIP */
 		work_cell(ws, "R"+[i], "");
+
 		/* rack number : 윈다 송장번호(C) */
 		work_cell(ws, "S"+[i], ws_origin["C"+[i]].w);
+
 		/* empty cells */
 	}	
 	return workbook;	
